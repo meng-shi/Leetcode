93c73143f4f8e720a4ae344b04e22da8f7a6c12e
meng-shi <shimeng09@gmail.com>
2026-01-17 22:15:17 -0800

    Time: 0 ms (100.00%) | Memory: 19.1 MB (20.68%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        n = len(s)
+        i = 0
+        
+        while i < n:
+            if i < n - 1 and dic[s[i]]  < dic[s[i+1]]:
+                sum += dic[s[i+1]] - dic[s[i]]
+                i +=2
+            else:
+                sum += dic[s[i]]
+                i += 1
+        return sum
+          
